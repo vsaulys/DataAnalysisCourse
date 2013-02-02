53dd0f8b7b555efc5d5a3f71669bb9f49d2624e3
Vincent Saulys <vincent.saulys@gmail.com>
2013-02-02 15:59:29 -0500

    updated some

--- Lecture-GettingData.R
+++ Lecture-GettingData.R
@@ -14,3 +14,16 @@ download.file(fileUrl, destfile=destFile, method='curl')
 cameraData <- read.csv(destFile)
 names(cameraData)
 str(cameraData)
+
+# now excel sheets
+require(xlsx)
+
+fileUrl <- "https://data.baltimorecity.gov/api/views/dz54-2aru/rows.xlsx?accessType=DOWNLOAD"
+destFile <- file.path(dataDir, "camera.xlsx")
+
+download.file(fileUrl, destfile=destFile, method="curl")
+
+cameraData <- read.xlsx2(destFile, sheetIndex=1)
+head(cameraData)
+str(cameraData)
+
