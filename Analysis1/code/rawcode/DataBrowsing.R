09c01afd46ccb1d950f79bac475378490d49b6d2
unknown <you@example.com>
2013-02-16 11:54:49 -0500

    one temp file cannot be ignored

--- Analysis1/code/rawcode/DataBrowsing.R
+++ Analysis1/code/rawcode/DataBrowsing.R
@@ -22,16 +22,34 @@ loansData$Interest.Rate <- as.numeric(sub("%", "", loansData$Interest.Rate))/100
 loansData$Debt.To.Income.Ratio <- as.numeric(sub("%", "", loansData$Debt.To.Income.Ratio))/100
 loansData$Loan.Length <- as.numeric(sub("months", "", loansData$Loan.Length))
 
+subset(loansData, Amount.Funded.By.Investors <= 0)
+loansData$Amount.Funded.By.Investors[loansData$Amount.Funded.By.Investors <= 0] <- 0
+subset(loansData, Amount.Funded.By.Investors <= 0)
+
+# set the reference level for the factors
+loansData$FICO.Range <- relevel(factor=loansData$FICO.Range, ref='640-644')
+
+summary(loansData)
 summary(loansData$Interest.Rate)
 summary(loansData$Debt.To.Income.Ratio)
 summary(loansData$Loan.Length)
 
-qqplot(loansData$Debt.To.Income.Ratio, loansData$Interest.Rate)
+ggplot(data=loansData, aes(x=Debt.To.Income.Ratio)) + geom_histogram(binwidth=.01)
 
-qplot(data=loansData, x=Debt.To.Income.Ratio, y=Interest.Rate)
+ggplot(data=loansData, aes(x=Debt.To.Income.Ratio, y=Interest.Rate, color=FICO.Range)) + geom_point()
 
-qplot(data=loansData, x=Debt.To.Income.Ratio)
-qplot(data=loansData, x=Interest.Rate)
+
+
+names(loansData)
+summary(loansData)
+
+
+
+lm1 <- lm( loansData$Interest.Rate ~ FICO.Range + Amount.Requested + Debt.To.Income.Ratio + Monthly.Income + Loan.Length, data=loansData)
+summary(lm1)
+
+anova(lm1)
+
 
 
 
